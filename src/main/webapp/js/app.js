b42425e1e236098b2deabef4d9ed460cca4ec72c
jsu <jingzans@gmail.com>
2014-11-24 21:05:45 -0800

    Angular home page setup.

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,9 +1,14 @@
 'use strict';
 
-var moliville = angular.module('moliville', [ 'ngRoute', 'ngResource' ])
-.config([ '$routeProvider', function($routeProvider) {
+var moliville = angular.module('moliville', [ 'ngRoute', 'ngResource' ]);
+
+moliville.config([ '$routeProvider', function($routeProvider) {
 	$routeProvider.when('/home', {
 		templateUrl : 'views/home.html',
+		controller : HomeController
+	});
+	$routeProvider.when('/products', {
+		templateUrl : 'views/products.html',
 		controller : HomeController
 	});
 	$routeProvider.otherwise({
